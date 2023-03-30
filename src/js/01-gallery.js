67fc8173643397ffe1986871200f4ea30a4e0ac0
ARTEMSHENDRYK <artemshendryk@gmail.com>
2023-03-30 14:21:27 +0300

    first try

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+//Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.reduce((acc, { preview, original, description }) =>
+  acc +=
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  </li>`, ''
+);
+
+galleryRef.innerHTML = galleryMarkup;
+
+// const gallery = new SimpleLightbox('.gallery a',
+//   {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//     captionPosition: 'bottom', //Default value
+//   }
+// );
